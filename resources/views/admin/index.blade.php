e8dcb8d40db78cfa7c1e78990bf15cdc1a14d817
diogogoncalves <d.m.goncalves640@gmail.com>
2026-03-27 15:17:46 +0000

    feat(admin): Add admin layout0 +

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('title', 'Dashboard')
+
+@section('breadcrumb')
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('content')
+    <section class="content">
+        <div class="container-fluid">
+
+        </div>
+    </section>
+@endsection
+
+@section('script')
+
+@endsection
