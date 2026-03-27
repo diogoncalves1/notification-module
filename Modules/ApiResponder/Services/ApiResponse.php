f66b40bfa30ce51b9585ccd25d4dda8108b83176
diogogoncalves <d.m.goncalves640@gmail.com>
2026-03-27 15:21:42 +0000

    feat(api-responder): Add Api Responder Module

---
+++ Modules/ApiResponder/Services/ApiResponse.php
@@ -0,0 +1,39 @@
+<?php
+namespace Modules\ApiResponder\Services;
+
+use Illuminate\Http\JsonResponse;
+
+class ApiResponse
+{
+    public static function success($data = null, string $message = '', int $code = 200, $additionals = null): JsonResponse
+    {
+        $data = [
+            'success' => true,
+            'message' => $message,
+            'data'    => $data,
+            'errors'  => null,
+        ];
+
+        if ($additionals !== null) {
+            $data['additionals'] = $additionals;
+        }
+
+        return response()->json($data, $code);
+    }
+
+    public static function error(string $message, $errors = null, int $code = 400, $additionals = null): JsonResponse
+    {
+        $data = [
+            'success' => false,
+            'message' => $message,
+            'data'    => null,
+            'errors'  => $errors,
+        ];
+
+        if ($additionals !== null) {
+            $data['additionals'] = $additionals;
+        }
+
+        return response()->json($data, $code);
+    }
+}
